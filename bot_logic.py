13e0d000cfee784fae217b4a48522dacadc88e29
_Sl1m_ <128687617+EgorTomi@users.noreply.github.com>
2025-10-04 13:32:16 +0500

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,26 @@
+import random
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+    
+def random_number_Game():
+    return random.randint(1, 10)
+
+def random_number():
+    return random.randint(1, 10)
+

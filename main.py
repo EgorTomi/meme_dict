4c93779479202fb8e38c13f96e276ed0ff2dc638
_Sl1m_ <128687617+EgorTomi@users.noreply.github.com>
2025-10-04 13:30:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,125 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
-while True:
-    word = input("Введите непонятное слово (большими буквами!): ")
-    if word in meme_dict.keys():
-        print(word, meme_dict[word])
+
+import telebot
+from bot_logic import gen_pass, gen_emodji, flip_coin, random_number_Game, random_number
+from telebot.types import ReactionTypeEmoji
+import random
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("8461369075:AAEEViNX0yUK4wkkQmFkvVQhJLbOvycu5yI")
+# Замени 'TOKEN' на токен твоего бота
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+
+@bot.message_handler(commands=['emodji'])
+def send_emodji(message):
+    emodji = gen_emodji()
+    bot.reply_to(message, {emodji})
+
+@bot.message_handler(commands=['coin'])
+def send_coin(message):
+    coin = flip_coin()
+    bot.reply_to(message, f"выпало: {coin}")
+
+
+@bot.message_handler(commands=['randomNumberGame']) #Не работает 
+def send_random_number_Game(message):
+    bot.reply_to(message, "Загадай число от 1 до 10")
+    if message.text.isdigit():
+        Num_Game = random_number()
+        if message.text == Num_Game:
+            bot.reply_to(message, "Ты угадал!")
+        else:
+            bot.reply_to(message, f"Ты не угадал, я загадал число {Num_Game}")
+
+@bot.message_handler(commands=['randomNumber'])
+def send_random_number(message):
+    R_Num = random_number()
+    bot.reply_to(message, f"Число: {R_Num}" )
+    
+# Send a reactions to all messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
+
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
+
+keys = ["1","2","3","4","5","6","7","8","9","0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+symbols = ["1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","\'","\"","/","\\",",",".",";",":"]
+
+def keyboard(key_type="Normal"):
+    markup = ReplyKeyboardMarkup(row_width=10)
+    if key_type == "Normal":
+        row = [KeyboardButton(x) for x in keys[:10]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in keys[10:20]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in keys[20:29]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in keys[29:]]
+        markup.add(*row)
+        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
+    elif key_type == "Symbols":
+        row = [KeyboardButton(x) for x in symbols[:10]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in symbols[10:20]]
+        markup.add(*row)
+        row = [KeyboardButton(x) for x in symbols[20:]]
+        markup.add(*row)
+        markup.add(KeyboardButton("Caps Lock"),KeyboardButton("Normal"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
     else:
-        print('Такого слова нет')
-        print('Хотите добавить в словарь')
-        if input().lower() == 'да':
-            value = input('Введите значение')
-            meme_dict[word] = value
+        row = [KeyboardButton(x.upper()) for x in keys[:10]]
+        markup.add(*row)
+        row = [KeyboardButton(x.upper()) for x in keys[10:20]]
+        markup.add(*row)
+        row = [KeyboardButton(x.upper()) for x in keys[20:29]]
+        markup.add(*row)
+        row = [KeyboardButton(x.upper()) for x in keys[29:]]
+        markup.add(*row)
+        markup.add(KeyboardButton("Normal"),KeyboardButton("Symbols"),KeyboardButton("🔙Delete"),KeyboardButton("✅Done"))
+    return markup
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id,"You can use the keyboard",reply_markup=keyboard())
+
+@bot.message_handler(func=lambda message:True)
+def all_messages(message):
+    if message.text == "✅Done":
+        markup = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id,"Done with Keyboard",reply_markup=markup)
+    elif message.text == "Symbols":
+        bot.send_message(message.from_user.id,"Special characters",reply_markup=keyboard("Symbols"))
+    elif message.text == "Normal":
+        bot.send_message(message.from_user.id,"Normal Keyboard",reply_markup=keyboard("Normal"))
+    elif message.text == "Caps Lock":
+        bot.send_message(message.from_user.id,"Caps Lock",reply_markup=keyboard("Caps"))
+    elif message.text == "🔙Delete":
+        bot.delete_message(message.from_user.id,message.message_id)
+    else:
+        bot.send_message(message.chat.id,message.text)
+
+bot.polling()
